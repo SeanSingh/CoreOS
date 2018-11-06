4fb8c2de131793d490c2b10a2f1b1493e18b8aa5
Sean Singh <sean@open.ngo>
2018-11-06 13:27:09 -0600

    trying to add pypy

--- coreos-python.sh
+++ coreos-python.sh
@@ -1,1 +1,8 @@
-wget https://bitbucket.org/pypy/pypy/downloads/pypy2-v6.0.0-linux64.tar.bz2
+
+
+
+VERSION=6.0.0
+GLIBC_VER=2.3.6-401834
+PYTHONV=3.5
+PACKAGE=pypy${PYTHONV}-${VERSION}-linux-x86_64-portable
+#wget https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3.5-6.0.0-linux_x86_64-portable.tar.bz2
--- coreos_activepython.sh
+++ coreos_activepython.sh
@@ -2,7 +2,7 @@
 
 # Path to this script: https://raw.githubusercontent.com/SeanSingh/coreos/master/coreos_activepython.sh
 VERSION=3.6.0.3600
-GLIBC_VER=2.3.6-40183
+GLIBC_VER=2.3.6-401834
 PACKAGE=ActivePython-${VERSION}-linux-x86_64-glibc-${GLIBC_VER}
 
 # make directory
@@ -12,13 +12,14 @@ wget http://downloads.activestate.com/ActivePython/releases/${VERSION}/${PACKAGE
 tar -xzvf ${PACKAGE}.tar.gz
 
 mv ${PACKAGE} apy && cd apy && ./install.sh -I /opt/python/
-
 printf "%s\n%s" "#! /usr/bin/bash" | sudo tee -a /etc/profile.d/opt-path.sh
 printf "%s\n%s" "PATH=\$PATH:/opt/bin:/opt/pypy/bin:/opt/python/bin" | sudo tee -a /etc/profile.d/opt-path.sh
 sudo chmod +x /etc/profile.d/opt-path.sh
 
-ln -sf /opt/python/bin/easy_install /opt/bin/easy_install
-ln -sf /opt/python/bin/pip /opt/bin/pip
-ln -sf /opt/python/bin/python /opt/bin/python
-ln -sf /opt/python/bin/python /opt/bin/python2
-ln -sf /opt/python/bin/virtualenv /opt/bin/virtualenv
+sudo ln -sf /opt/python/bin/easy_install /opt/bin/easy_install
+sudo ln -sf /opt/python/bin/pip3 /opt/bin/pip3
+sudo ln -sf /opt/python/bin/pip3 /opt/bin/pip
+sudo ln -sf /opt/python/bin/python3 /opt/bin/python
+sudo ln -sf /opt/python/bin/python3 /opt/bin/python3
+sudo ln -sf /opt/python/bin/pyvenv /opt/bin/virtualenv
+sudo ln -sf /opt/python/bin/pyvenv /opt/bin/pyvenv
