2da90dc322db21463be83836ca0ed64a812b6cf6
Sean Singh <sean@open.ngo>
2018-11-06 09:04:21 -0600

    Updated deployment scripts.

---
+++ coreos-python.sh
@@ -0,0 +1,1 @@
+wget https://bitbucket.org/pypy/pypy/downloads/pypy2-v6.0.0-linux64.tar.bz2
--- iso-inst.sh
+++ iso-inst.sh
@@ -1,4 +1,7 @@
 #Download this file with: curl -OL https://tinyurl.com/coreos-iso
 # curl -OL https://raw.githubusercontent.com/SeanSingh/coreos/master/coreos-install.sh
 curl -OL https://raw.githubusercontent.com/SeanSingh/coreos/master/minimal-ignition.json
-sh coreos-install -d /dev/sda -C stable -i minimal-ignition.json
+curl -OL https://raw.githubusercontent.com/SeanSingh/coreos/master/coreos-python.sh
+coreos-install -d /dev/sda -C stable -i minimal-ignition.json
+
+systemctl reboot
