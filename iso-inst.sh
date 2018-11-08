a0a591a0ed2fb33efe4e9bbdbe3d2ce9af6f3f0e
Sean Singh <sean@open.ngo>
2018-11-07 23:01:57 -0600

    updated path and command for installing, now works on liveCDs.

--- iso-inst.sh
+++ iso-inst.sh
@@ -1,5 +1,6 @@
 #Download this file with: curl -OL https://tinyurl.com/coreos-iso
 # curl -OL https://raw.githubusercontent.com/SeanSingh/coreos/master/coreos-install.sh
-curl -OL https://raw.githubusercontent.com/SeanSingh/coreos/master/minimal-ignition.json
-sudo coreos-install -d /dev/sda -C stable -i minimal-ignition.json
+wget https://raw.githubusercontent.com/SeanSingh/coreos/master/minimal-ignition.json
+wget https://raw.githubusercontent.com/SeanSingh/coreos/master/coreos-install
+sudo /bin/bash coreos-install -d /dev/sda -C stable -i minimal-ignition.json
 echo "systemctl reboot"
